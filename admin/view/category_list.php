3c0d5b42d3fa8dabe346c092d49752739356249d
Ankit-0040 <ankitssingh1912@gmail.com>
2023-08-01 09:48:26 +0530

    update

---
+++ admin/view/category_list.php
@@ -0,0 +1,124 @@
+<?php echo $header; ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  
+ 
+<div id="content">
+  <div class="breadcrumb">
+    <?php foreach ($breadcrumbs as $breadcrumb) { ?>
+    <?php echo $breadcrumb['separator']; ?><a href="<?php echo $breadcrumb['href']; ?>"><?php echo $breadcrumb['text']; ?></a>
+    <?php } ?>
+  </div>
+  <?php if ($error_warning) { ?>
+  <div class="warning"><?php echo $error_warning; ?></div>
+  <?php } ?>
+  <?php if ($success) { ?>
+  <div class="success"><?php echo $success; ?></div>
+  <?php } ?>
+  <div class="box">
+    <div class="heading">
+      <h1><img src="view/image/category.png" alt="" /> <?php echo $heading_title; ?></h1>
+      <div class="buttons"><a href="<?php echo $repair; ?>" class="button"><?php echo $button_repair; ?></a><a onclick="if(event.ctrlKey == false) {pushHistory('catalog/category/insert');return false;}" href="<?php echo $insert; ?>" class="button"><?php echo $button_insert; ?></a><a onclick="$('#form').submit();" class="button"><?php echo $button_delete; ?></a></div>
+    </div>
+    
+    
+    <div class = "container">
+         <div class="size w-50 h-auto">
+<div class="border p-3"> 
+<div class= "row">
+ <div class="card text-dark bg-light mb-3 mx-3 w-25 h-25" >
+  <div class="card-header">Total Order</div>
+  <div class="card-body">
+    <h5 class="card-title">Today's Order</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+ <div class="card text-dark bg-light mb-3 mx-3 w-25 h-25">
+  <div class="card-header">Total Sales</div>
+  <div class="card-body">
+    <h5 class="card-title">Today's Sale's</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
+</div>
+<div class="column mt-3">
+     <canvas id="lineChart"></canvas>
+    </div>
+    </div>
+    </div>
+</div>
+
+
+
+</div>
+    <div class="content">
+      <form action="<?php echo $delete; ?>" method="post" enctype="multipart/form-data" id="form">
+        <table class="list">
+          <thead>
+            <tr>
+              <td width="1" style="text-align: center;"><input type="checkbox" onclick="$('input[name*=\'selected\']').attr('checked', this.checked);" /></td>
+              <td class="left"><?php echo $column_name; ?></td>
+              <td class="right"><?php echo $column_sort_order; ?></td>
+              <td class="right"><?php echo $column_action; ?></td>
+            </tr>
+          </thead>
+          <tbody>
+            <?php if ($categories) { ?>
+            <?php foreach ($categories as $category) { ?>
+            <tr>
+              <td style="text-align: center;"><?php if ($category['selected']) { ?>
+                <input type="checkbox" name="selected[]" value="<?php echo $category['category_id']; ?>" checked="checked" />
+                <?php } else { ?>
+                <input type="checkbox" name="selected[]" value="<?php echo $category['category_id']; ?>" />
+                <?php } ?></td>
+              <td class="left"><?php echo $category['name']; ?></td>
+              <td class="right"><?php echo $category['sort_order']; ?></td>
+              <td class="right"><?php foreach ($category['action'] as $action) { ?>
+                [ <a href="<?php echo $action['href']; ?>"><?php echo $action['text']; ?></a> ]
+                <?php } ?></td>
+            </tr>
+            <?php } ?>
+            <?php } else { ?>
+            <tr>
+              <td class="center" colspan="4"><?php echo $text_no_results; ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
+      </form>
+      <div class="pagination"><?php echo $pagination; ?></div>
+    </div>
+  </div>
+</div>
+
+<script>
+//line
+var ctxL = document.getElementById("lineChart").getContext('2d');
+var myLineChart = new Chart(ctxL, {
+  type: 'line',
+  data: {
+    labels: ["12:00 am", "5:00 am", "10:00 am", "03:00 pm", "08:00 pm", "12:00 am"],
+    datasets: [{
+      label: "Order's Per Day",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        'rgba(105, 0, 132, .2)',
+      ],
+      borderColor: [
+        'rgba(200, 99, 132, .7)',
+      ],
+      borderWidth: 2
+    },
+    
+    ]
+  },
+  options: {
+    responsive: true
+  }
+});
+</script>
+<?php echo $footer; ?>
